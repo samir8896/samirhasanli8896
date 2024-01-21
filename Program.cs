a2d39d0799ab2ceb8bfd2e5072cb3cc5d476db56
Soltanov Nijat <NijatjS@code.local>
2024-01-21 19:51:07 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Week2app2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Day number for result");
+            int daynumber =int.Parse( Console.ReadLine());
+            if (daynumber == 1)
+            {
+                Console.WriteLine("go to home");
+            }
+
+            else if (daynumber == 2)
+            {
+                Console.WriteLine("go to home");
+
+            }
+            else if (daynumber == 3)
+            {
+                Console.WriteLine("go to work");
+            }
+            else if (daynumber == 4)
+            {
+                Console.WriteLine("go to sport");
+            }
+            else if (daynumber == 5)
+            {
+                Console.WriteLine("go to football");
+
+
+            }
+            else
+            {
+                Console.WriteLine("go to football");
+
+            }
+        }
+        }
+      
+            }
+        
+    
